e8064c006a84c2f73e7aba6cacaacee520f2e872
dan-sl <daniel.tritscher@skipline.com>
2026-06-16 13:21:32 +1200

    Updated firewood interaction logic
    Inverted the logic to no longer return early. The old behaviour would
    prevent the base method from being called which in turn prevented any
    behaviours from being called (when the shift key was not pressed).
    The new logic still prevents the interaction with the desired blocks
    when applicable.
    
    Note: There could be side affects if behaviours on this item were coded
    expecting the item to filter interactions for them. However if that is
    the case, that should probably be fixed.

--- Item/ItemFirewood.cs
+++ Item/ItemFirewood.cs
@@ -10,16 +10,17 @@ namespace Vintagestory.GameContent
         
         public override void OnHeldInteractStart(ItemSlot slot, EntityAgent byEntity, BlockSelection blockSel, EntitySelection entitySel, bool firstEvent, ref EnumHandHandling handling)
         {
-            if (blockSel == null || byEntity?.World == null || !byEntity.Controls.ShiftKey) return;
-
-            BlockPos onBlockPos = blockSel.Position;
-            Block block = byEntity.World.BlockAccessor.GetBlock(onBlockPos);
-
-            if (block is BlockFirepit || block is BlockPitkiln || block is BlockClayOven)
+            if (blockSel != null && byEntity?.World != null && byEntity.Controls.ShiftKey)
             {
-                // Prevent placing firewoodpiles when trying to construct firepits
-                return;
-            } 
+                BlockPos onBlockPos = blockSel.Position;
+                Block block = byEntity.World.BlockAccessor.GetBlock(onBlockPos);
+
+                if (block is BlockFirepit || block is BlockPitkiln || block is BlockClayOven)
+                {
+                    // Prevent placing firewoodpiles when trying to construct firepits
+                    return;
+                }
+            }
 
             base.OnHeldInteractStart(slot, byEntity, blockSel, entitySel, firstEvent, ref handling);
         }
